4f9f6dea8dc1697d4c5004a30152c3e610fe1f00
drifape <drif20500@gmail.com>
2025-06-05 17:05:47 +0700

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -45,5 +45,4 @@ def process_build_url(url: str) -> list:
     try:
         return json.loads(result.stdout)
     except json.JSONDecodeError as e:
-        raise RuntimeError(f"Ошибка разбора JSON из Lua: {e}")
         raise RuntimeError(f"Ошибка разбора JSON из Lua: {e}"
